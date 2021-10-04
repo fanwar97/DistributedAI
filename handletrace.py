b2fc30305b6bf8bea4f0cdb7900a5b8dbd45bdbd
Hai <hai.hm182918@sis.hust.edu.vn>
2021-10-04 13:19:52 +0700

    Revert "Added inital generated attack domains"
    This reverts commit 3c6c4e46396ddd987a7aaebcdb7a7875ec8e511e.

--- handletrace.py
+++ handletrace.py
@@ -1,99 +1,94 @@
-"""
-Testing trace
-"""
-import os
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.embeddings import Embedding
+from keras.layers.recurrent import LSTM
 
-os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.4/bin")
-# pylint: disable=C0413
+from pandas import read_csv, concat
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, roc_auc_score
+
+import tldextract
 import numpy as np
 import tensorflow as tf
-import tldextract
-from keras.preprocessing.sequence import pad_sequences
-from pandas import read_csv
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
 
-# from print_log import print_log
-# from message import *
-# from utils import *
-# from model import *
+from print_log import print_log
+from message import *
+from utils import *
+from model import *
 
-def do_evaluate(matrix, probs):
-    """Evaluate predictions"""
+
+def do_evaluate(y, probs):
     print(probs)
 
     tmp = probs.flatten()
-    with np.nditer(tmp, op_flags=["readwrite"]) as iterator:
-        for value in iterator:
-            if value > 0.5:
-                value[...] = 1
-            else:
-                value[...] = 0
+    for i in range(len(tmp)):
+        if tmp[i] > 0.5:
+            tmp[i] = 1
+        else:
+            tmp[i] = 0
     tmp = tmp.astype(int)
     print(tmp)
-    true_neg, false_pos, false_neg, true_pos = confusion_matrix(matrix.to_numpy().tolist(),
-                                                                tmp.tolist()).ravel()
+    tn, fp, fn, tp = confusion_matrix(
+        y.to_numpy().tolist(), tmp.tolist()).ravel()
 
-    precision = true_pos/(true_pos+false_pos)
-    recall = true_pos/(true_pos+false_neg)
+    precision = tp/(tp+fp)
+    recall = tp/(tp+fn)
     f1score = (2*precision*recall)/(precision+recall)
-    print(f"TP: {true_pos}\nTN: {true_neg}\nFP: {false_pos}\nFN: {false_neg}\n")
-    print(f"FP rate: {false_pos/(false_pos+true_pos)*100}%\n"
-            f"FN rate: {false_neg/(false_neg+true_neg)*100}%\n")
-    print(f"Accuracy: {(true_pos+true_neg)/(true_pos+true_neg+false_pos+false_neg)}")
+    print("TP: {}\nTN: {}\nFP: {}\nFN: {}\n".format(tp, tn, fp, fn))
+    print("FP rate: {}%\nFN rate: {}%\n".format(fp/(fp+tp)*100, fn/(fn+tn)*100))
+    print("Accuracy: {}".format((tp+tn)/(tp+tn+fp+fn)))
     print(f"Precision: {precision}")
     print(f"Recal: {recall}")
     print(f"F1-score: {f1score}")
 
-
-def cal_avg(a_weight, list_of_weight, num_client_this_round):
-    """Calculate average"""
+def cal_avg(list_of_weight, num_client_this_round):
+    global avg_weight
     for i in range(len(list_of_weight[0])):
-        for j in range(1, num_client_this_round-1):
+        for j in range(1, len(list_of_weight)):
             list_of_weight[0][i] += list_of_weight[j][i]
         list_of_weight[0][i] /= num_client_this_round
-    a_weight = list_of_weight[0]
-    return a_weight
+    avg_weight = list_of_weight[0]
 
 
 avg_weight = []
 validChars = {chr(i+45): i for i in range(0, 78)}
-MAXLEN = 127
+maxlen = 127
 
-current_model = tf.keras.models.load_model("saved_model\\model_500_char_based")
+current_model = tf.keras.models.load_model("simple_LSTM_model")
 current_model.summary()
 
 initial_w = current_model.get_weights()
-train_domain = read_csv("benign_500.txt", names=['domain'])
+train_domain = read_csv("benign.txt", names=['domain'], nrows=10000)
 train_domain['tld'] = [tldextract.extract(d).domain for d in train_domain['domain']]
-train_domain = train_domain[~train_domain['tld'].str.contains(r'\`|-\.')]
+train_domain = train_domain[~train_domain['tld'].str.contains('\`|-\.')]
 train_domain = train_domain.drop_duplicates()
 train_domain['label'] = 0
 train_domain = train_domain.sample(frac=1).reset_index(drop=True)
-tld_matrix, label_matrix = train_domain['tld'], train_domain['label']
-tld_matrix = [[validChars[y] for y in x] for x in tld_matrix]
-tld_matrix = pad_sequences(tld_matrix, maxlen=MAXLEN)
+X, y = train_domain['tld'], train_domain['label']
+X = [[validChars[y] for y in x] for x in X]
+X = pad_sequences(X, maxlen=maxlen)
 
-tld_train, tld_test, label_train, label_test = train_test_split(tld_matrix, label_matrix,
-                                                                test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-guess = current_model.predict(tld_matrix)
-do_evaluate(label_matrix, guess)
+probs = current_model.predict(X)
+do_evaluate(y, probs)
 
-current_model.fit(tld_train, label_train, batch_size=16, epochs=1)
+current_model.fit(X_train, y_train, batch_size=16, epochs=1)
 
 
 trained_weights = current_model.get_weights()
 
-weight_list = []
-weight_list.append(initial_w)
-weight_list.append(trained_weights)
+list_of_weight = []
+list_of_weight.append(initial_w)
+list_of_weight.append(trained_weights)
 
-avg_weight = cal_avg(avg_weight, weight_list, 2)
+cal_avg(list_of_weight, 2)
 current_model.set_weights(avg_weight)
 
 
 print("MODEL AFTER CAL AVG")
-probs_2 = current_model.predict(tld_matrix)
+probs_2 = current_model.predict(X)
 
-do_evaluate(label_matrix, probs_2)
+do_evaluate(y, probs_2)
